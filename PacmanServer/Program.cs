47c2269924bd4a85841044eb473ba4d0efdb800b
didtmdwns1130 <ysj113077@gmail.com>
2025-10-09 00:04:49 +0900

    Add TCP server feature (Server.cs)

--- PacmanServer/Program.cs
+++ PacmanServer/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PacmanServer
 {
-    internal class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        [STAThread]
+        private static void Main()
         {
+            try
+            {
+                var server = new Server();
+                // 7777 포트에서 대기 (원하면 숫자 바꿔도 됨)
+                server.StartAsync(7777).GetAwaiter().GetResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[SERVER] Fatal: " + ex);
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ PacmanServer/Server.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanServer
+{
+    public class Server
+    {
+        private TcpListener listener;
+
+        public async Task StartAsync(int port = 7777)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+            listener.Start();
+            Console.WriteLine($"[SERVER] Listening on port {port}...");
+
+            while (true)
+            {
+                var client = await listener.AcceptTcpClientAsync();
+                _ = HandleClientAsync(client);
+            }
+        }
+
+        private async Task HandleClientAsync(TcpClient client)
+        {
+            Console.WriteLine("[SERVER] Client connected");
+
+            using (var stream = client.GetStream())
+            {
+                var buffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                var msg = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine($"[SERVER] Received: {msg}");
+
+                var reply = Encoding.UTF8.GetBytes("Hello Client!");
+                await stream.WriteAsync(reply, 0, reply.Length);
+            }
+
+            Console.WriteLine("[SERVER] Client disconnected");
+        }
+    }
+}
